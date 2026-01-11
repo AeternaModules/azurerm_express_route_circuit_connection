03d02b98c6a12ec87796e9e6f110e00728090a9c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:52:59 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "express_route_circuit_connections" {
+  description = "All express_route_circuit_connection resources"
+  value       = azurerm_express_route_circuit_connection.express_route_circuit_connections
+}
+
