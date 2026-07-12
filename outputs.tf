0725a0b6dc594da399db0be3e5fa5c6e9cb7df29
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,26 @@
-output "express_route_circuit_connections" {
-  description = "All express_route_circuit_connection resources"
-  value       = azurerm_express_route_circuit_connection.express_route_circuit_connections
-  sensitive   = true
-}
 output "express_route_circuit_connections_address_prefix_ipv4" {
-  description = "List of address_prefix_ipv4 values across all express_route_circuit_connections"
-  value       = [for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : v.address_prefix_ipv4]
+  description = "Map of address_prefix_ipv4 values across all express_route_circuit_connections, keyed the same as var.express_route_circuit_connections"
+  value       = { for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : k => v.address_prefix_ipv4 }
 }
 output "express_route_circuit_connections_address_prefix_ipv6" {
-  description = "List of address_prefix_ipv6 values across all express_route_circuit_connections"
-  value       = [for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : v.address_prefix_ipv6]
+  description = "Map of address_prefix_ipv6 values across all express_route_circuit_connections, keyed the same as var.express_route_circuit_connections"
+  value       = { for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : k => v.address_prefix_ipv6 }
 }
 output "express_route_circuit_connections_authorization_key" {
-  description = "List of authorization_key values across all express_route_circuit_connections"
-  value       = [for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : v.authorization_key]
+  description = "Map of authorization_key values across all express_route_circuit_connections, keyed the same as var.express_route_circuit_connections"
+  value       = { for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : k => v.authorization_key }
   sensitive   = true
 }
 output "express_route_circuit_connections_name" {
-  description = "List of name values across all express_route_circuit_connections"
-  value       = [for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : v.name]
+  description = "Map of name values across all express_route_circuit_connections, keyed the same as var.express_route_circuit_connections"
+  value       = { for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : k => v.name }
 }
 output "express_route_circuit_connections_peer_peering_id" {
-  description = "List of peer_peering_id values across all express_route_circuit_connections"
-  value       = [for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : v.peer_peering_id]
+  description = "Map of peer_peering_id values across all express_route_circuit_connections, keyed the same as var.express_route_circuit_connections"
+  value       = { for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : k => v.peer_peering_id }
 }
 output "express_route_circuit_connections_peering_id" {
-  description = "List of peering_id values across all express_route_circuit_connections"
-  value       = [for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : v.peering_id]
+  description = "Map of peering_id values across all express_route_circuit_connections, keyed the same as var.express_route_circuit_connections"
+  value       = { for k, v in azurerm_express_route_circuit_connection.express_route_circuit_connections : k => v.peering_id }
 }
 
